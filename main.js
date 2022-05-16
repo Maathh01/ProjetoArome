679dfd2c543aa574804c2b932dc097bce7bd794e
Matheus <70244782+Maathh01@users.noreply.github.com>
2022-05-16 15:46:48 -0300

    feat add home page (#2)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let search = document.querySelector('.search-box');
 
-document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
     cart.classList.remove('active');
     user.classList.remove('active');
@@ -8,7 +8,7 @@ document.querySelector('#search-icon').onclick = () =>{
 
 let cart = document.querySelector('.cart-box');
 
-document.querySelector('#cart-icon').onclick = () =>{
+document.querySelector('#cart-icon').onclick = () => {
     cart.classList.toggle('active');
     search.classList.remove('active');
     user.classList.remove('active');
@@ -16,8 +16,33 @@ document.querySelector('#cart-icon').onclick = () =>{
 
 let user = document.querySelector('.user-box');
 
-document.querySelector('#user-icon').onclick = () =>{
+document.querySelector('#user-icon').onclick = () => {
     user.classList.toggle('active');
     cart.classList.remove('active');
     search.classList.remove('active');
 }
+
+// Swiper
+var swiper = new Swiper(".new-arrival", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 0,
+        },
+        568: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
